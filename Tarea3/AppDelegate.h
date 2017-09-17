7d332e291684e329aa4718fd9641cf49f49c235f
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-09-16 19:28:29 -0500

    Initial Commit

---
+++ Tarea3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tarea3
+//
+//  Created by Silvestre Garcia on 9/16/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tarea3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tarea3
+//
+//  Created by Silvestre Garcia on 9/16/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
